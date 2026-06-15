6ab5dc1bd52e2f7272cce211426caee92bf5d73a
haketag <wunjokpy@gmail.com>
2026-06-15 22:00:04 +0700

    add to the photo

--- app/src/main/java/com/example/cityflowbkk/navigation/NavGraph.kt
+++ app/src/main/java/com/example/cityflowbkk/navigation/NavGraph.kt
@@ -1,8 +1,1 @@
-package com.example.cityflowbkk.navigation
 
-import androidx.compose.runtime.Composable
-
-@Composable
-fun CityFlowNavGraph() {
-    MainScreen()
-}
--- app/src/main/java/com/example/cityflowbkk/navigation/Screen.kt
+++ app/src/main/java/com/example/cityflowbkk/navigation/Screen.kt
@@ -3,7 +3,7 @@ package com.example.cityflowbkk.navigation
 sealed class Screen(val route: String) {
     data object Home : Screen("home")
     data object Route : Screen("route")
-    data object Map : Screen("map")
+    data object Map   : Screen("map")
     data object Station : Screen("station")
     data object Profile : Screen("profile")
 }
